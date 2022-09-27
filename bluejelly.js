be6d052517a898146be23b5a7645e30cf113cbbd
Ashouma <86941401+Ashouma@users.noreply.github.com>
2022-09-27 20:23:54 +0900

    Add files via upload

--- bluejelly.js
+++ bluejelly.js
@@ -70,7 +70,7 @@ var BlueJelly = function(){
     
     return navigator.bluetooth.requestDevice({
       acceptAllDevices:true, // 全てのデバイスを対象にスキャンを実施する
-      optionalServices:['7530f362-3daa-11ed-b878-0242ac120002']}
+      optionalServices:[7530f362-3daa-11ed-b878-0242ac120002]}
       )
     .then(device => {
       this.bluetoothDevice = device;
